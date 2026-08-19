d8959685a6217e2b8546c5be214a34e3d6f27d15
Sebastian <Setho2104@oslomet.no>
2026-08-19 10:14:15 +0200

    feat: add ones_returnsZeroWhenNoOnes test

---
+++ src/test/java/com/sebastian/yatzy/YatzyScorerTests.java
@@ -0,0 +1,16 @@
+package com.sebastian.yatzy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+public class YatzyScorerTests{
+
+    @Test
+    public void ones_returnsZeroWhenNoOnes() {
+        int [] dice = {2, 2, 4, 5, 3, 6};
+        int result = YatzyScorer.ones(dice);
+        assertEquals(0, result);
+    }
+}
